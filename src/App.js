ca63095b2b8ca1be411be20b4fa3e662c8e9d7a2
TRSKZK <75508554+TRSKZK@users.noreply.github.com>
2021-05-25 19:11:29 +0300

    second increment emplemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import { concat, interval, Subject } from 'rxjs';
+
+import { startWith, scan, takeWhile} from 'rxjs/operators';
+
+const Container = styled.div`
+display:flex;
+margin:30px;
+justify-content:center;
+
+`
+
+const myInterval$ = interval(1000).pipe(
+  startWith(0),
+  scan(time => time += 1),
+  takeWhile(time => time <= 60 )
+)
+
+
+const minutes$ = concat(myInterval$)
+
+// const action$ = new Subject()
 
 function App() {
+  let [sec, setSec] = useState(0)
+  let [start, setStart] = useState(false);
+  
+  useEffect(() => {
+    
+    if(start === true) myInterval$.subscribe(setSec)
+     
+    console.log(start);
+    return ()=> myInterval$.unsubscribe()
+  },[])
+
+  
+  const handleClickFunction = () => {
+    return start === false ? setStart(true)  : setStart(false)
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>{sec}</h1>
+      <button onClick={()=> setStart(true)}>Start</button>
+    </Container>
   );
 }
 
